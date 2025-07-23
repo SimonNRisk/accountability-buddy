b545a8023f3102154026b5dfb6016358fd154fee
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-23 18:38:34 -0400

    setup constants and types

---
+++ accountability-buddy/src/lib/constants.ts
@@ -0,0 +1,11 @@
+import { Activity } from "@/types/activityTypes";
+
+export const dailyActivities: Activity[] = [
+  { id: 1, title: "Exercise", completed: false },
+  { id: 2, title: "Talk to loved ones", completed: false },
+  { id: 3, title: "Meditate", completed: false },
+  { id: 4, title: "Journal", completed: false },
+  { id: 5, title: "Study", completed: false },
+  { id: 6, title: "Leetcode", completed: false },
+  { id: 7, title: "Outreach", completed: false },
+];
