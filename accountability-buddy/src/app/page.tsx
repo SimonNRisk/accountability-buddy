a65571327e4a6a493f3715dfb2bd320e164309ee
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-26 21:24:42 -0400

    log firebase data on fe

--- accountability-buddy/src/app/page.tsx
+++ accountability-buddy/src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { getDocs } from "firebase/firestore";
 
 import { ChecklistItem } from "@/components/ChecklistItem";
 import { dailyActivities } from "@/lib/constants";
 import { useChecklistItem } from "@/hooks/useChecklistItem";
+import { colRef } from "@/lib/firebase";
 
 export default function Home() {
+  getDocs(colRef).then((snapshot) => {
+    console.log(snapshot.docs[0].data());
+  });
   const { items, toggleItem } = useChecklistItem(dailyActivities);
   return (
     <ul>
---
+++ accountability-buddy/src/lib/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBVxvTnUlcu-Tt1bzU2JdHSzbneDWcEtaM",
+  authDomain: "accountability-buddy-305c1.firebaseapp.com",
+  projectId: "accountability-buddy-305c1",
+  storageBucket: "accountability-buddy-305c1.firebasestorage.app",
+  messagingSenderId: "395132344421",
+  appId: "1:395132344421:web:6fa15ab6baec1d37dece8c",
+  measurementId: "G-RC9VWY4J71",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//database
+export const db = getFirestore(app);
+
+// collection ref
+export const colRef = collection(db, "users");
