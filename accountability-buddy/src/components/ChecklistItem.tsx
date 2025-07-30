ad317e5518d8ae4a20435667ccf8f59291a21bf7
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-29 22:25:49 -0400

    feat: rough draft for removing items from activities

--- accountability-buddy/src/components/ChecklistItem.tsx
+++ accountability-buddy/src/components/ChecklistItem.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 type ChecklistItemProps = {
   id: number;
@@ -13,14 +14,30 @@ export function ChecklistItem({
   completed,
   onToggle,
 }: ChecklistItemProps) {
+  const [showListItemAction, setShowListItemActions] = useState(false);
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => onToggle(id, !completed)}
-      ></input>
-      <span className={completed ? "line-through" : ""}>{title}</span>
-    </li>
+    <div
+      className="flex flex-row gap-x-2"
+      onMouseEnter={() => setShowListItemActions(true)}
+      onMouseLeave={() => setShowListItemActions(false)}
+    >
+      <button
+        className={`transition-opacity duration-400 ${
+          showListItemAction ? "opacity-100" : "opacity-0"
+        }`}
+        onClick={() => console.log("clicked")}
+      >
+        x
+      </button>
+
+      <li>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => onToggle(id, !completed)}
+        ></input>
+        <span className={completed ? "line-through" : ""}>{title}</span>
+      </li>
+    </div>
   );
 }
