aa8841c93d8bc8f6aa13e25944ea8523d55fb9d0
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-08-01 19:50:03 -0400

    feat(cleanup): WIP: modal to add an item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,28 @@
 "use client";
 import { useState } from "react";
-import { Plus, Minus } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Checklist } from "@/components/Checklist";
 import { useChecklist } from "@/hooks/useChecklist";
+import { AddItemModal } from "@/components/AddItemModal";
 
 export default function Home() {
-  const [inputVisible, setInputVisible] = useState(false);
-  const [newItemTitle, setNewItemTitle] = useState("");
+  const [showAddItemModal, setShowAddItemModal] = useState(false);
+  const { items, toggleItem } = useChecklist();
 
-  const { items, toggleItem, addItem } = useChecklist();
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await addItem(newItemTitle);
-    setNewItemTitle("");
-    setInputVisible(false);
-  };
+  const onClose = () => setShowAddItemModal(false);
 
   return (
     <>
-      <form className="flex flex-row" onSubmit={handleSubmit}>
-        {inputVisible ? (
-          <>
-            <input
-              className="bg-white text-black px-2 py-1 rounded"
-              placeholder="Enter an activity"
-              value={newItemTitle}
-              onChange={(e) => setNewItemTitle(e.target.value)}
-              autoFocus
-            />
-            <button
-              type="button"
-              onClick={() => setInputVisible(false)}
-              className="text-xl px-2 cursor-pointer"
-            >
-              <Minus />
-            </button>
-          </>
-        ) : (
-          <button
-            type="button"
-            onClick={() => setInputVisible(true)}
-            className="text-xl px-2 cursor-pointer"
-          >
-            <Plus />
-          </button>
-        )}
-      </form>
+      <button
+        type="button"
+        onClick={() => setShowAddItemModal(true)}
+        className="text-xl px-2 cursor-pointer"
+      >
+        <Plus />
+      </button>
 
       <Checklist items={items} onToggle={toggleItem} />
+      {showAddItemModal && <AddItemModal onClose={onClose} />}
     </>
   );
 }
