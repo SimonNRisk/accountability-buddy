ef9f6d2d1e8faf6b6842a23f1be4728182609816
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-08-01 19:53:22 -0400

    fix(cleanup): position modal to be fixed inset centered

--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -17,20 +17,22 @@ export const AddItemModal = ({ onClose }: addItemModalProps) => {
   };
 
   return (
-    <div className="flex flex-col justify-center gap-y-4 bg-black">
-      <h4>Add a new activity to your daily checklist</h4>
-      <form className="flex flex-row" onSubmit={handleSubmit}>
-        <input
-          className="bg-white text-black px-2 py-1 rounded"
-          placeholder="Play badminton"
-          value={newItemTitle}
-          onChange={(e) => setNewItemTitle(e.target.value)}
-          autoFocus
-        />
-      </form>
-      <button type="button" onClick={onClose}>
-        Cancel
-      </button>
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs">
+      <div className="flex flex-col justify-center gap-y-4 bg-black">
+        <h4>Add a new activity to your daily checklist</h4>
+        <form className="flex flex-row" onSubmit={handleSubmit}>
+          <input
+            className="bg-white text-black px-2 py-1 rounded"
+            placeholder="Play badminton"
+            value={newItemTitle}
+            onChange={(e) => setNewItemTitle(e.target.value)}
+            autoFocus
+          />
+        </form>
+        <button type="button" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };
