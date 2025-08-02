89974b970366684a85bfbfe35272f5f0022075fb
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-08-01 21:48:00 -0400

    feat(login): add logout button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,18 @@
 
 import { useChecklist } from "@/hooks/useChecklist";
 import { useTimeLeft } from "@/hooks/useTimeLeft";
+import { useRouter } from "next/navigation";
+import { clearLoginMode } from "@/lib/loginMode";
 
 export const Header = () => {
   const { items, completedItems } = useChecklist();
   const { timeLeft } = useTimeLeft();
+  const router = useRouter();
+
+  const logout = () => {
+    clearLoginMode;
+    router.push("/login");
+  };
   return (
     <div className="sticky top-0 shadow-z-50">
       <div className="flex flex-row justify-between border-b">
@@ -16,6 +24,9 @@ export const Header = () => {
           TODO: PROGRESS BAR {(completedItems.length / items.length).toFixed(2)}
         </p>
         <p>Time left today: {timeLeft}</p>
+        <button className="cursor-pointer" onClick={logout}>
+          Logout
+        </button>
       </div>
     </div>
   );
