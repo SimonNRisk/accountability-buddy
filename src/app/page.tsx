2f2aff32b6cc72dd596986a12eed5f714c169526
Simon Risk <simonnrisk@gmail.com>
2025-10-31 12:01:50 -0400

    rev: fix formatting, change z score by 1 for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,11 @@ export default function Home() {
       <div className="max-w-4xl mx-auto px-4 py-8">
         {isEditor && (
           <div className="flex justify-center mb-6">
-            <button type="button" onClick={() => setShowAddItemModal(true)} className="text-xl px-4 py-2 cursor-pointer bg-gray-800 hover:bg-gray-700 rounded-lg border border-gray-600 transition-colors duration-300">
+            <button
+              type="button"
+              onClick={() => setShowAddItemModal(true)}
+              className="text-xl px-4 py-2 cursor-pointer bg-gray-800 hover:bg-gray-700 rounded-lg border border-gray-600 transition-colors duration-300"
+            >
               <Plus className="inline mr-2" />
               Add Item
             </button>
--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -18,14 +18,23 @@ export const AddItemModal = ({ onClose }: addItemModalProps) => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs">
+    <div className="fixed inset-0 z-51 flex items-center justify-center backdrop-blur-xs">
       <div className="flex flex-col justify-center items-center gap-y-8 bg-black p-8 border border-white rounded-lg shadow-[0_0_20px_4px_rgba(255,255,255,0.4)]">
         <h4>Add a new activity to your daily checklist</h4>
         <form className="flex flex-row w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col w-full">
-            <input className="text-white px-2 py-1 rounded border shadow-[0_0_20px_4px_rgba(255,255,255,0.4)] focus:outline-none focus:ring-1 focus:ring-white" placeholder="Play badminton" value={newItemTitle} onChange={(e) => setNewItemTitle(e.target.value)} />
+            <input
+              className="text-white px-2 py-1 rounded border shadow-[0_0_20px_4px_rgba(255,255,255,0.4)] focus:outline-none focus:ring-1 focus:ring-white"
+              placeholder="Play badminton"
+              value={newItemTitle}
+              onChange={(e) => setNewItemTitle(e.target.value)}
+            />
             <div className="flex justify-end mt-4">
-              <button type="button" onClick={onClose} className="cursor-pointer p-2 border rounded-lg shadow-[0_0_20px_4px_rgba(255,255,255,0.4)] mx-4">
+              <button
+                type="button"
+                onClick={onClose}
+                className="cursor-pointer p-2 border rounded-lg shadow-[0_0_20px_4px_rgba(255,255,255,0.4)] mx-4"
+              >
                 Cancel
               </button>
               <button
